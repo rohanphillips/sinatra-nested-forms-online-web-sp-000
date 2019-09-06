83a434f9541f556f227d6703a0468122c6ece4f2
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-06 14:37:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require './environment'
+require 'rack'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+    get '/new' do
+      erb :'/pirates/new'
+    end
 
+    post '/pirates' do
+      #binding.pry
+      #params.inspect
+      #erb :'/pirates/show'
+    end
   end
 end
