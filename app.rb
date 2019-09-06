7b9eb34bf7be4a4976b09ac3dfcdc9d59526df19
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-06 14:52:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,11 @@ module FormsLab
     end
 
     post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
+      @ships = Ship.all
       erb :'/pirates/show'
     end
   end
